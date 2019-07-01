c0994c937df9b26b33e302a9c9df8be91e7982c3
Jundi Ye <rundiye93@gmail.com>
2019-07-01 14:54:59 +0200

    second commit

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
 'use strict';
+
+function main (){
+    var mainElement = document.querySelector('#site-main');
+
+    function buildDom(html){
+        mainElement.innerHTML = html;
+        return mainElement;
+    };
+
+    function createSplashScreen(){
+        var splashScreen = buildDom(`
+        <section>
+            <h1>Eternal Enemies</h1>
+            <button>Start</button>
+        </section>
+        `);
+    };
+
+    createSplashScreen();
+
+    function createGameScreen(){
+
+    };
+
+    function createGameOverScreen(){
+
+    };
+
+};
+
+window.addEventListener('load', main);
